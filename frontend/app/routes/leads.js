c9fdbef50e44b82bd67f400d500ec0ab83566455
adamyarger <yargeradam@gmail.com>
2015-08-09 16:48:53 -0500

    show lead is working but the link is not

--- frontend/app/routes/leads.js
+++ frontend/app/routes/leads.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.find('lead');
+  model() {
+    return this.store.findAll('lead');
   }
 });
