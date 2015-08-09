441db5dfe4feec15e5667c36fa49368b93311c86
adamyarger <yargeradam@gmail.com>
2015-08-09 11:15:43 -0500

    adds api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'ember#bootstrap'
   get '/*path' => 'ember#bootstrap'
+
+  namespace :api do
+  	namespace :v1 do
+  		resources :leads
+  	end
+  end
 end
