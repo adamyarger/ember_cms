0b7a431116e3f0b95fe43bdd9ae001dd96e5614b
adamyarger <yargeradam@gmail.com>
2015-08-11 17:53:01 -0500

    delete action wokrs

--- frontend/app/controllers/lead.js
+++ frontend/app/controllers/lead.js
@@ -4,6 +4,9 @@ export default Ember.Controller.extend({
 	actions: {
 		saveChanges: function() {
 			this.get('model').save();
+		},
+		delete: function() {
+			this.get('model').destroyRecord()
 		}
 	}
 });
